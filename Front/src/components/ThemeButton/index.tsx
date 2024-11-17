f345822467775cefed90fa2409b1ade38b4f091d
Pietro Bucker <pietro_177@hotmail.com>
2024-11-17 15:42:31 -0300

    Styles: estilizando searchBar e themeButton, criado variaveis de da paleta no root

--- Front/src/components/ThemeButton/index.tsx
+++ Front/src/components/ThemeButton/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from './themeButton.module.css'
 
 export default function ThemeButton({ ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
+    className={styles['theme-button']}
     {...props}
     >themeButton</button>
   )
