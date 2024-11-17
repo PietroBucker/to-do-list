525733c7686a5c2bdbf0f70bf6d9ba25d179ad82
Pietro Bucker <pietro_177@hotmail.com>
2024-11-17 15:52:19 -0300

    Feat: funcionalidade do themebutton 'OK'

--- Front/src/components/ThemeButton/index.tsx
+++ Front/src/components/ThemeButton/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './themeButton.module.css'
 
-export default function ThemeButton({ ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+export default function ThemeButton() {
+  const [theme, setTheme] = React.useState<string>('dark')
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  }, [])
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+    document.documentElement.setAttribute('data-theme', newTheme)
+  }
   return (
     <button
     className={styles['theme-button']}
-    {...props}
+    onClick={toggleTheme}
     >themeButton</button>
   )
 }
