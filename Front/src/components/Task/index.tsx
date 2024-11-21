9630ecd7e77cdb0b0861f1493df328ff23a0fec6
Pietro Bucker <pietro_177@hotmail.com>
2024-11-20 21:15:21 -0300

    Modificado component Task

--- Front/src/components/Task/index.tsx
+++ Front/src/components/Task/index.tsx
@@ -2,12 +2,12 @@ import StyledButton from '../StyledButton';
 import styles from './task.module.css';
 import pencil from '../../assets/pencil.svg';
 import trash from '../../assets/trash-svgrepo-com 1.svg';
-export default function Tasks() {
+
+
+export default function Tasks({children}: any) {
   return (
     <div className={styles['task-container']}>
-      <p>Name Task</p>
-      <p>Limit Date</p>
-      <p>Cost</p>
+      {children}
       <div className={styles['task-container-buttons']}>
         <StyledButton><img src={pencil} alt='pencil' /></StyledButton>
         <StyledButton><img src={trash} alt="trash" /></StyledButton>
