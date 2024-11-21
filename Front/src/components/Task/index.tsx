80b18ca839fc23183e794aefa24ed1dec5e1cd5e
Pietro Bucker <pietro_177@hotmail.com>
2024-11-20 23:08:35 -0300

    ADD/STYLE: add chama de form modal em task, add action para form para separa entre save e edit

--- Front/src/components/Task/index.tsx
+++ Front/src/components/Task/index.tsx
@@ -2,16 +2,30 @@ import StyledButton from '../StyledButton';
 import styles from './task.module.css';
 import pencil from '../../assets/pencil.svg';
 import trash from '../../assets/trash-svgrepo-com 1.svg';
+import React from 'react';
+import Form from '../Form';
 
 
-export default function Tasks({children}: any) {
+export default function Tasks({desc ,children }: any) {
+  const editModalRef = React.useRef<HTMLDialogElement>(null)
+  const [isOpen, setIsOpen] = React.useState(false)
   return (
-    <div className={styles['task-container']}>
+    <div
+      className={styles['task-container']}
+      onMouseOver={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+      >
       {children}
       <div className={styles['task-container-buttons']}>
-        <StyledButton><img src={pencil} alt='pencil' /></StyledButton>
+        <StyledButton
+        onClick={() => editModalRef.current?.showModal()}
+        ><img src={pencil} alt='pencil' /></StyledButton>
         <StyledButton><img src={trash} alt="trash" /></StyledButton>
       </div>
+      <dialog open={isOpen} className={styles.dialog_description}>
+        <p className={styles.description}>{desc}</p>
+      </dialog>
+      <Form ref={editModalRef} useRef={editModalRef} action={'edit'} />
     </div>
   )
 }
