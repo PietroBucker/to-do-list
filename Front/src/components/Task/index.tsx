e73c6236b1bcedf1a34c8e45d0fa50139b17499c
Pietro Bucker <pietro_177@hotmail.com>
2024-11-24 17:14:46 -0300

    FEAT: nova funcionalidade de alterar ordem com drag and drop

--- Front/src/components/Task/index.tsx
+++ Front/src/components/Task/index.tsx
@@ -6,36 +6,41 @@ import React from 'react';
 import Form from '../Form';
 import { deleteTask } from '../../helpers';
 
-
-export default function Tasks({ desc, children, loading, id, over1000 }: any) {
-  const editModalRef = React.useRef<HTMLDialogElement>(null)
-  const [isOpen, setIsOpen] = React.useState(false)
-
-  
+export default function Tasks({ 
+  item, 
+  children, 
+  loading, 
+  id, 
+  over1000, 
+  draggableProps, 
+  dragHandleProps, 
+  innerRef 
+}: any) {
+  const editModalRef = React.useRef<HTMLDialogElement>(null);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <div
+      {...draggableProps}
+      {...dragHandleProps}
+      ref={innerRef}
       className={`${styles['task-container']} ${over1000 ? styles['task-over-1000'] : ''}`}
       onMouseOver={() => setIsOpen(true)}
       onMouseLeave={() => setIsOpen(false)}
     >
       {children}
       <div className={styles['task-container-buttons']}>
-        <StyledButton
-          onClick={() => editModalRef.current?.showModal()}
-        >
+        <StyledButton onClick={() => editModalRef.current?.showModal()}>
           <img src={pencil} alt='pencil' />
         </StyledButton>
-        <StyledButton
-        onClick={() => deleteTask(id, loading)}
-        >
+        <StyledButton onClick={() => deleteTask(id, loading)}>
           <img src={trash} alt="trash" />
         </StyledButton>
       </div>
       <dialog open={isOpen} className={styles.dialog_description}>
-        <p className={styles.description}>{desc}</p>
+        <p className={styles.description}>{item.desc}</p>
       </dialog>
       <Form ref={editModalRef} useRef={editModalRef} action={'edit'} loading={loading} id={id} />
     </div>
-  )
+  );
 }
--- Front/src/pages/Home.tsx
+++ Front/src/pages/Home.tsx
@@ -4,46 +4,88 @@ import { filterData, getTasks } from "../helpers";
 import Tasks from "../components/Task";
 import SearchBar from "../components/SearchBar";
 import ThemeButton from "../components/ThemeButton";
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 import StyledButton from "../components/StyledButton";
-import cross from '../assets/cross.svg'
+import cross from '../assets/cross.svg';
 import Form from "../components/Form";
+import { DragDropContext, Droppable, Draggable, DropResult } from "@hello-pangea/dnd";
 
 export default function Home() {
-    const [search, setSearch] = useState<string>('')
-    const [data, setData] = useState<TasksResponse[]>([])
-    const modalFormRef = React.useRef<HTMLDialogElement>(null);
-    const [loading, setLoading] = useState<boolean>(true)
+  const [search, setSearch] = useState<string>('');
+  const [data, setData] = useState<TasksResponse[]>([]);
+  const modalFormRef = React.useRef<HTMLDialogElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        getTasks(setData, setLoading)
-    }, [loading])
-    
-    const filtred = filterData(data, search) 
+  useEffect(() => {
+    getTasks(setData, setLoading);
+  }, [loading]);
 
-    return (
-        <div className={styles['home-container']}>
-            <h1>TODO LIST</h1>
-            <div className={styles['home-container-search']}>
-                <SearchBar value={search} onChange={({ target }) => setSearch(target.value)} />
-                <ThemeButton />
-            </div>
-            <div className={styles['home-container-tasks']}>
-            {loading ? <h1>Carregando...</h1> : ''}
-                {!filtred ? '' : filtred.map((item) => (
-                    <Tasks key={item.id} desc={item.description} loading={setLoading} id={item.id} over1000={item.cost as number >= 1000 ? true : false}>
-                        <div className={styles.task_item}>{item.task_name}</div>
-                        <div className={styles.task_item}>{item.limit_date}</div>
-                        <div className={styles.task_item}>{item.cost}</div>
-                    </ Tasks>))}
-                <StyledButton 
-                    onClick={() => modalFormRef.current?.showModal()}
-                    className={styles['fixed-button']}>
-                    <img src={cross} alt="" />
-                </StyledButton>
-                <Form ref={modalFormRef} useRef={modalFormRef} action={'save'} loading={setLoading} />
-            </div>
+  const filtred = filterData(data, search);
 
-        </div>
-    )
+  const handleDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const reorderedData = [...data];
+    const [movedItem] = reorderedData.splice(result.source.index, 1);
+    reorderedData.splice(result.destination.index, 0, movedItem);
+
+    setData(reorderedData);
+  };
+
+  return (
+    <div className={styles['home-container']}>
+      <h1>TODO LIST</h1>
+      <div className={styles['home-container-search']}>
+        <SearchBar value={search} onChange={({ target }) => setSearch(target.value)} />
+        <ThemeButton />
+      </div>
+      <div className={styles['home-container-tasks']}>
+        {loading ? <h1>Carregando...</h1> : ''}
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <Droppable droppableId="tasks">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={styles['tasks-list-droppable']}
+              >
+                {filtred &&
+                  filtred.map((item, index) => (
+                    <Draggable key={item.id} draggableId={item.id.toString()} index={index} >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className={styles['tasks-container-draggable']}
+                        >
+                          <Tasks
+                            item={item}
+                            loading={setLoading}
+                            id={item.id}
+                            over1000={item.cost as number >= 1000}
+                          >
+                            <div className={styles.task_item_container}>{item.task_name}</div>
+                            <div className={styles.task_item_container}>{item.limit_date}</div>
+                            <div className={styles.task_item_container}>{item.cost}</div>
+                          </Tasks>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        <StyledButton
+          onClick={() => modalFormRef.current?.showModal()}
+          className={styles['fixed-button']}
+        >
+          <img src={cross} alt="" />
+        </StyledButton>
+        <Form ref={modalFormRef} useRef={modalFormRef} action={'save'} loading={setLoading} />
+      </div>
+    </div>
+  );
 }
