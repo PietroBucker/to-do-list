fa93cf09d98c9e05270c7fcfb4bd6cdd1f33e054
Pietro Bucker <pietro_177@hotmail.com>
2024-11-24 21:26:05 -0300

    config cors

--- Back/src/ApiService.ts
+++ Back/src/ApiService.ts
@@ -99,7 +99,7 @@ export default class ApiService implements IApiService {
 
     private setupMiddleware(): void {
         const corsOptions = {
-            origin: 'https://to-do-list-nine-lime-73.vercel.app', 
+            origin: ['https://to-do-list-nine-lime-73.vercel.app/', 'https://to-do-list-zvcz.vercel.app/tasks'], 
             methods: ['GET', 'POST', 'PUT', 'DELETE'], 
             allowedHeaders: ['Content-Type', 'Authorization'],
         };
