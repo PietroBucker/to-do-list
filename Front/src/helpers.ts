223298d782e73f21415f06301878d83e18f11297
Pietro Bucker <pietro_177@hotmail.com>
2024-11-24 23:04:02 -0300

    testando o put erro de cors

--- Front/src/helpers.ts
+++ Front/src/helpers.ts
@@ -31,6 +31,7 @@ export const saveTask = async (data:TasksResponse, setMessage:reactDispatch<stri
 }
 
 export const editTask = async (data:TasksResponse, id:number ,setMessage:reactDispatch<string>) => {
+    console.log("helpler")
     const response = await fetch(`https://to-do-list-zvcz.vercel.app/tasks/${id}`, {
         method: 'PUT',
         headers: {
@@ -45,7 +46,6 @@ export const editTask = async (data:TasksResponse, id:number ,setMessage:reactDi
     // else {
     //     setMessage('Tarefa atualizada com sucesso')
     // }
-    console.log("helpler")
 }
 
 
