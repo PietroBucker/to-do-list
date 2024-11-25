a513efe37c807ea506991df64f6e77699a31c3c5
Pietro Bucker <pietro_177@hotmail.com>
2024-11-24 21:52:42 -0300

    teste cors

--- Back/src/ApiService.ts
+++ Back/src/ApiService.ts
@@ -99,7 +99,7 @@ export default class ApiService implements IApiService {
 
     private setupMiddleware(): void {
         const corsOptions = {
-            origin: ['https://to-do-list-h5a87x4zc-pietro-buckers-projects.vercel.app/', 'https://to-do-list-nine-lime-73.vercel.app/', 'https://to-do-list-git-main-pietro-buckers-projects.vercel.app/'], 
+            origin: 'to-do-list-zvcz.vercel.app',
             methods: ['GET', 'POST', 'PUT', 'DELETE'], 
             allowedHeaders: ['Content-Type', 'Authorization'],
         };
